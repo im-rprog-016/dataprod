3ed935afb8c36967904e9ff21551d24ab1bdc780
Ioan Moldovan <ioan.moldovan@tora.com>
2015-04-26 12:06:51 +0300

    Shiny app

---
+++ server.R
@@ -0,0 +1,41 @@
+#  Shiny application exemplifying model clustering on the Iris dataset
+#
+# Reference: 
+#  Chris Fraley, Adrian E. Raftery, T. Brendan Murphy, and Luca Scrucca (2012) mclust Version 4 for R: Normal Mixture Modeling for
+#  Model-Based Clustering, Classification, and Density Estimation Technical Report No. 597, Department of Statistics, University of
+#  Washington
+#
+#  Chris Fraley and Adrian E. Raftery (2002) Model-based Clustering, Discriminant Analysis and Density Estimation Journal of the
+#  American Statistical Association 97:611-631
+#
+# Start the appplication in the current working directory with
+# 		runApp()
+# or start it in a subfoder
+# 		runApp("/path/to/folder")
+#
+
+pckg = c("mclust", "gridExtra") 
+is.installed <- function(mypkg){ is.element(mypkg, installed.packages()[,1]) } 
+for(i in 1:length(pckg)) { if (!is.installed(pckg[i])) { install.packages(pckg[i]) } }
+
+library(mclust)
+
+shinyServer(function(input, output, session) {
+
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    iris[, c(input$xcol, input$ycol)]
+  })
+
+  clusters <- reactive({
+    Mclust(selectedData(), modelNames=input$model)
+  })
+
+  output$plotClusters <- renderPlot({
+    plot(clusters(), what='classification')
+    #plot(selectedData(),
+    #     col = clusters()$cluster,	
+    #     pch = 20, cex = 3)
+    #points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,33 @@
+#  Shiny application exemplifying model clustering on the Iris dataset
+#
+# Reference: 
+#  Chris Fraley, Adrian E. Raftery, T. Brendan Murphy, and Luca Scrucca (2012) mclust Version 4 for R: Normal Mixture Modeling for
+#  Model-Based Clustering, Classification, and Density Estimation Technical Report No. 597, Department of Statistics, University of
+#  Washington
+#
+#  Chris Fraley and Adrian E. Raftery (2002) Model-based Clustering, Discriminant Analysis and Density Estimation Journal of the
+#  American Statistical Association 97:611-631
+#
+# Start the appplication in the current working directory with
+# 		runApp()
+# or start it in a subfoder
+# 		runApp("/path/to/folder")
+#
+
+pckg = c("mclust", "gridExtra") 
+is.installed <- function(mypkg){ is.element(mypkg, installed.packages()[,1]) } 
+for(i in 1:length(pckg)) { if (!is.installed(pckg[i])) { install.packages(pckg[i]) } }
+
+library(mclust)
+
+shinyUI(pageWithSidebar(
+  headerPanel('Model Clustering on Iris dataset'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(iris), selected=names(iris)[[3]]),
+    selectInput('ycol', 'Y Variable', names(iris), selected=names(iris)[[4]]),
+    selectInput('model', 'Model', mclust.options("emModelNames"), selected="VEV")
+  ),
+  mainPanel(
+    plotOutput('plotClusters')
+  )
+))
